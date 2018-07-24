093a3984f81cca50f41f6def440104ba08a8f1c3
Dylan Carty <dylancarty8@gmail.com>
2018-07-24 15:35:54 +0000

    Create binary_search.py
    Binary Search:
    Added the binary search algorithm to the main branch, this algorithm uses a recursive approach in my implementation.

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+# This is an example of a binary search algorithm in Python using a recursive/divide and conquer approach.
+# In a binary search algorithm we start with a midpoint of a sorted list. Once we have this midpoint we then
+# use it to find out if the item we want to find is in the lower or upper half of the list. This is done in O(log n) time.
+# It is important to remember that binary search is done on a sorted list or structure.
+
+def binarySearch(mylist, item):
+    if len(mylist)==0:
+        return False
+    else:
+        midpoint=len(mylist)//2
+        if myslist[midpoint]==item:
+            return True
+        else:
+            if item<mylist[midpoint]:
+                return binarySearch(mylist[:midpoint], item)
+            else:
+                return binarySearch(mylist[midpoint+1], item)
+                
+testlist = [1,2,3,4,5,6,7,8,9,10]
+print(binarySearch(testlist, 3)
